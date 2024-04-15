460022ce211e5226d10d8ff52913fc9e79035bd3
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-04-15 10:19:18 +0900

    Add files via upload

---
+++ Ch3/Code03-01.py
@@ -0,0 +1,14 @@
+katok =[]
+
+def add_data(friend):
+
+    katok.append(None)
+    kLen=len(katok)
+    katok[kLen-1]=friend
+
+while True:
+
+    data=input("추가할 데이터 -> ")
+    add_data(data)
+    print(katok)
+
